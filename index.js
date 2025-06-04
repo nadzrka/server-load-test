c4ada5f411e126a75722f8f76433160dfb755ead
nadzira <nadziraka19@gmail.com>
2025-06-05 01:28:29 +0700

    fix

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
-// index.js
-const express = require("express");
-const app = express();
-const path = require("path")
-const PORT = 3000;
+export default async function handler(req, res) {
+  try {
+    const response = await fetch("https://nadzira-load-test.glitch.me/100kb");
+    const data = await response.json();
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Halo dari Express!");
-});
-
-app.get('/10kb', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', '10kb.html'));
-});
-
-app.get('/100kb', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', '100kb.html'));
-});
-
-app.get('/1000kb', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', '1000kb.html'));
-});
-
-app.listen(PORT, () => {
-  console.log(`Server berjalan di http://localhost:${PORT}`);
-});
+    // Kirim data ke client
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Gagal fetch data" });
+  }
+}
