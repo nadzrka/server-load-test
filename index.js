b0f93e49776e8230a9a4cbd02243870854a86b01
nadzira <nadziraka19@gmail.com>
2025-06-05 01:02:56 +0700

    add

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // index.js
 const express = require("express");
 const app = express();
+const path = require("path")
 const PORT = 3000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Halo dari Express!");
+app.get('/10kb', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'html', '10kb.html'));
 });
 
 app.listen(PORT, () => {
